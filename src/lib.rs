3a8b2758a25a1080a2cd5c267112ef9a01a42aae
namit-bluwbee <189242095+namit-bluwbee@users.noreply.github.com>
2025-12-18 15:04:23 +0000

    add `upload_raw` function to use a `Vec<u8>` binary directly instead of reading a file,

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,5 @@ mod transfer;
 mod test_serial_port;
 
 pub use crate::default::reset;
-pub use crate::image::{list, upload, test, erase};
+pub use crate::image::{list, upload, test, erase, upload_raw};
 pub use crate::transfer::SerialSpecs;
